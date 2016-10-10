f89ed4222b19c17f1f68c2f642275341fc54e1e1
Jack Vasquez <Jack.Vasquez@DATALINK.COM>
2016-10-09 22:14:50 -0400

    fixed routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,24 +2,23 @@ import { NgModule }      from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { RouterModule }   from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import  { HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent }  from './app.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [ 
-             BrowserModule, 
-             FormsModule, 
-             HttpModule,
-             RouterModule.forRoot([ 
-      { path: '', component: HomeComponent }
-    ]) 
-           ],
+    BrowserModule, 
+    FormsModule, 
+    HttpModule,
+    AppRoutingModule
+  ],
   declarations: [ 
-                  AppComponent,
-                  HomeComponent 
-                ],  
+    AppComponent,
+    HomeComponent 
+  ],  
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
