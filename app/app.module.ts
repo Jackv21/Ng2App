a15d6125d9d6e936dc21ac1b208a8422ccea2ef7
Jack Vasquez <Jack.Vasquez@DATALINK.COM>
2016-10-09 21:17:11 -0400

    layout done

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule }      from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import  { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
-import { CustomerComponent, CustomersComponent, CustomerService }  from './customer/index';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule, HttpModule ],
-  declarations: [ AppComponent, 
-                  CustomerComponent,
-                  CustomersComponent ],
-  providers: [CustomerService],
+  imports: [ 
+             BrowserModule, 
+             FormsModule, 
+             HttpModule 
+           ],
+  declarations: [ 
+                  AppComponent, 
+                  LayoutComponent 
+                ],  
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: __moduleName,
+    selector: 'layout',
+    templateUrl: 'layout.component.html'
+})
+export class LayoutComponent implements OnInit{ 
+   ngOnInit(){
+       
+   }
+}
